4852e7ae40bc74ebb29a5843307cf267bcc05b89
M-Filipov <milko.filipov1@abv.bg>
2014-06-21 18:02:52 +0300

    Testing nmber generators

--- TestGenerator.rb
+++ TestGenerator.rb
@@ -9,15 +9,44 @@ class TestGenerator
 	def genSimpleHex diff=0 
 		twoNill = 0
 		while true
-			test = (0x0005 + Random.rand(0xFFFF+1)).to_s(16)
-			test.to_s.each_char do |c|
+			temp = (0x0005 + Random.rand(0xFFFF+1)).to_s(16)
+			temp.to_s.each_char do |c|
 				if c == '0'
 					twoNill += 1
 				end
 			end
-			return test if twoNill == diff and test.length == 4
+			return temp if twoNill == diff and temp.length == 4
 			twoNill = 0
 		end
 
 	end
+	
+	def genShiftNum diff=0, odd=true 
+		temp = ((3+diff*2) + Random.rand((3+diff*2-1)))*2 + odd = (odd ? 1 : 0)
+	end
+
 end
+
+
+tg = TestGenerator.new 0
+
+
+# puts tg.genSimpleHex
+
+orig = tg.genSimpleHex 2
+
+while true
+	insert = ((tg.genSimpleHex 3).to_i >> 8).to_s(16)
+	break if insert.length == 1 and insert != '0'
+end
+
+temp = tg.genShiftNum 0, false
+
+a = (orig.hex | (insert.hex << temp)).to_s(16)
+
+
+puts "a = ? …………."
+puts "int orig = #{orig}"
+puts "int insert = 000#{insert}"
+puts "int a = orig | (insert << #{temp})"
+puts "a = #{a}"
